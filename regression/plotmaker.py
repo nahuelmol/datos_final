98817ef56305d392d12c57868d56f3e452142b9c
unknown <kindermolina98@gmail.com>
2025-11-27 18:37:09 -0300

    regression plot is done but it needs a revision

--- regression/plotmaker.py
+++ regression/plotmaker.py
@@ -5,24 +5,32 @@ def vectors_plot(X_train, y_train, filename, model):
     X = X_train.sort_index().to_numpy()
     y = y_train.to_numpy()
 
+    pred = model.predict(X_train)
+    ilok = X_train.iloc[model.support_]
+    xv = X[model.support_]
+    preds = np.delete(pred, -1)
 
-    print(type(model.support_))
-    print(X)
-    print(y[model.support_])
-    #print(y[0,1])
-    
-    """
+    #print(xv[:,0].size)
+    #print(ilok.iloc[:,0].size)
+
     plt.figure(figsize=(10, 6))
-    plt.scatter(
-        X[model.support_],
-        y[model.support_],
-        facecolor=None,
-        edgecolor='Green',
-        s=80,
-        label='data'
-    )
+    plt.plot(
+            xv[:,0],
+            preds,
+            color="g",
+            lw=2,
+            label="model"
+            )
+    for i in range(1):
+        plt.scatter(
+            X[model.support_][:,i],
+            y[model.support_],
+            facecolor=None,
+            edgecolor='Green',
+            s=80,
+            label='data'
+        )
     plt.savefig(filename, dpi=300)
     plt.close()
-    """
 
 
